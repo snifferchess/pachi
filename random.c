ee757738296083f477da77170049119e3b31c03a
lemonsqueeze <lemonsqueeze@gmx.com>
2018-01-17 00:23:26 +0100

    mingw speedup: don't use thread-local storage in fast_random()
    Turns out mingw-w64's thread-local storage is quite slow on some platforms,
    given how much fast_random() is used it pays to work around it.
    
    Still slower than linux build, but much better now.

--- random.c
+++ random.c
@@ -1,13 +1,101 @@
+#define DEBUG
+#include "debug.h"
 #include <stdio.h>
-
+#include <pthread.h>
+#include <assert.h>
 #include "random.h"
 
 
 /* Simple Park-Miller for floating point; LCG as used in glibc and other places */
 
+
+/***************************************************************************************************/
+#ifdef _WIN32
+
+/* Use custom thread-local storage,
+ * mingw-w64's __thread is painfully slow on some platforms. */
+
+#define MAX_THREADS 16
+static unsigned long pmseed[MAX_THREADS] = { 29264, 29264, 29264, 29264, 29264, 29264, 29264, 29264,
+					     29264, 29264, 29264, 29264, 29264, 29264, 29264, 29264 };
+
+static unsigned int threads[MAX_THREADS] = { 0, };
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static int register_thread(unsigned int id);
+
+static int
+get_thread_id()
+{
+	unsigned int id = GetCurrentThreadId();  /* Faster than pthread_self() */
+	for (int i = 0; i < MAX_THREADS; i++)
+		if (threads[i] == id)
+			return i;
+	
+	return register_thread(id);  /* Add new thread */
+}
+
+static int
+register_thread(unsigned int id)
+{
+	pthread_mutex_lock(&mutex);
+	for (int i = 0; i < MAX_THREADS; i++)
+		if (!threads[i]) {
+			if (DEBUGL(4)) fprintf(stderr, "fast_random(): registering thread %i (%#x)\n", i, id);
+			threads[i] = id;
+			pthread_mutex_unlock(&mutex);
+			return i;
+		}
+	assert(0);  return 0;
+}
+
+/* Called at thread-exit time */
+void
+fast_random_unregister_thread()
+{
+	unsigned int id = GetCurrentThreadId();
+	pthread_mutex_lock(&mutex);  
+	for (int i = 0; i < MAX_THREADS; i++)
+		if (threads[i] == id) {
+			if (DEBUGL(4)) fprintf(stderr, "fast_random(): unregistering thread %i (%#x)\n", i, id);
+			threads[i] = 0;
+			pthread_mutex_unlock(&mutex);
+			return;
+		}
+	assert(0);
+}
+
+
+void
+fast_srandom(unsigned long seed_)
+{
+        int id = get_thread_id();
+	pmseed[id] = seed_;
+}
+
+unsigned long
+fast_getseed(void)
+{
+        int id = get_thread_id();
+	return pmseed[id];
+}
+
+uint16_t
+fast_random(unsigned int max)
+{
+        int id = get_thread_id();
+	pmseed[id] = ((pmseed[id] * 1103515245) + 12345) & 0x7fffffff;
+	return ((pmseed[id] & 0xffff) * max) >> 16;
+}
+
+
+#else
+/***************************************************************************************************/
 #ifndef NO_THREAD_LOCAL
 
+/* Default, use thread-local storage. */
 static __thread unsigned long pmseed = 29264;
+
 
 void
 fast_srandom(unsigned long seed_)
@@ -38,7 +126,9 @@ fast_frandom(void)
 	return p.f - 1.0f;
 }
 
+
 #else
+/***************************************************************************************************/
 
 /* Thread local storage not supported through __thread,
  * use pthread_getspecific() instead. */
@@ -88,4 +178,5 @@ fast_frandom(void)
 	return p.f - 1.0f;
 }
 
+#endif
 #endif
